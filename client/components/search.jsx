03a58b8abfc4b8320adb959e1409b724a04a14f9
Ryan Gliever <ryangliever@gmail.com>
2016-06-18 20:26:34 -0700

    Add `handleChange` function
    Calls `Videos.get` with the subreddit name in a `setTimeout` — called
    when the user stops typing for half a second.

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import $ from 'jquery';
+import Videos from 'lib/Videos.js';
 var classNames = require('classnames');
 
 var Search = React.createClass({
@@ -11,12 +13,23 @@ var Search = React.createClass({
       focused: true
     });
   },
+
+  handleChange: function(event) {
+    var name = $(event.target).val();
+
+    /* Wait until user stops typing to execute get */
+    if (this._timeout) clearTimeout(this._timeout);
+    this._timeout = setTimeout(function () {
+      Videos.get(name);
+    }, 500);
+  },
+
   render: function() {
   	var searchClasses = classNames('search-wrap', {focused: this.state.focused});
   	var hintClasses = classNames('transition', {invisible: this.state.focused})
     return (
       <div className={searchClasses}>
-      	<input type='text' className='search' onClick={this.handleFocus}></input>
+      	<input type='text' className='search' onClick={this.handleFocus} onChange={this.handleChange} />
       	<p className={hintClasses}>enter a subreddit</p>
       </div>
     );
