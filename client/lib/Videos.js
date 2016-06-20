f106643ad8978abaacf7c635a9bda6c70c56c1ab
Ryan Gliever <ryangliever@gmail.com>
2016-06-19 17:47:20 -0700

    Get the src from the iframe part of the post json.
    TODO: this only works currently with youtube videos; we’ll have to
    check for other sources (vimeo, etc.) or something else in the future.

--- client/lib/Videos.js
+++ client/lib/Videos.js
@@ -13,6 +13,13 @@ var Videos = {
     }.bind(this)).fail(function (error) {
       $(window).trigger('set-videos-error', error);
     });
+  },
+
+  getUrl: function (index) {
+    var iframe = this.current ? this.current.data.children[index].data.media_embed.content : null;
+    if (!iframe) return null;
+    var src = iframe.match(/src="(\S+)"/)[1].concat('&autoplay=1');
+    return src;
   }
 };
 
